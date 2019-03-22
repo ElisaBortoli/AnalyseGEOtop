857b5ad0054977a1c54919833be91f6b4d982ba7
ElisaBortoli <elisa.bortoli3@gmail.com>
2019-03-22 17:07:07 +0100

    making GEOtop_Plot_Point.R (1D) working

--- inst/GEOtop_Plot_1D/GEOtop_Plot_Point.R
+++ inst/GEOtop_Plot_1D/GEOtop_Plot_Point.R
@@ -8,14 +8,7 @@
 #------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Select simulation folder (wpath <- ...):
-# wpath <-  "C:/Users/CBrida/Desktop/Simulations_GEOtop/CRYOMON_sim_157_v002/"     
-# wpath <-  "C:/Users/GBertoldi/Documents/Simulations_local/Kaltern_veg/Kaltern_veg_004"
-# wpath  <-  "C:/Users/GBertoldi/Documents/Simulations_local/Snow_Cryomon/CRYOMON_sim_157_v021/"
-
-# wpath  <-  "C:/Users/GBertoldi/Documents/Simulations_local/Montacini_elisa/1D/Matsch_P2_Ref_001"
-#wpath  <-  "C:/Users/GBertoldi/OneDrive - Scientific Network South Tyrol/Simulations/Johannes/1D/B2_P2_Giacomo/B2_BeG_012S"
-wpath  <- "C:/Users/GBertoldi/Documents/Simulations_local/Snow_Cryomon/1D/CRYOMON_sim_1D_204_v004"
-wpath <-  "C:/Users/GBertoldi/Documents/Simulations_local/Snow_Cryomon/3D/CRYOMON_sim_157_v022"
+wpath <- "/home/elisa/Scrivania/MHPC/CRYOMON_sim_1D_204_v004"
 
 #- Install and import packages and functions ----------------------------------------------------------------------------------------------------------
 
@@ -50,9 +43,7 @@ if(!require("data.table"))
 
 library(ggplot2)
 
-# Loads functions (until this is not in an R package)
-source("../../R/GEOtop_ReadPointData_Generalized.R") # this function is available in folder AnalyseGEOtop/ins/GEOtop_Plot_1D
-source("../../R/GEOtop_Plot_loop.R") # function to plot as .jpeg same outputs of several points
+library(AnalyseGeotop)
 
 #- Read "tabs/point" files of simulations -----------------------------------------------------------------------------------------------------------------
 
@@ -63,15 +54,9 @@ keywords <- declared.geotop.inpts.keywords(wpath = wpath)$Keyword
 # Import data
 # to add here a user confirmation if you load existing data
 # it does not work any more in the more complete case of soil_info = T
-
-#if (file.exists(file.path(wpath,"PointOut.RData"))) {
-#  load(file.path(wpath,"PointOut.RData"))
-#} else {
-  out <- GEOtop_ReadPointData_Generalized(wpath,soil_info = F,save_rData = F)
-#}
+out <- GEOtop_ReadPointData_Generalized(wpath,soil_info = F,save_rData = F)
 
 # Import points coordinates
-
 xpoints <- get.geotop.inpts.keyword.value("CoordinatePointX",wpath=wpath,numeric=T)
 ypoints <- get.geotop.inpts.keyword.value("CoordinatePointY",wpath=wpath,numeric=T)
 
@@ -82,7 +67,7 @@ n_points_available <- as.numeric(length(xpoints))
 # Select the sigle point to plot
 # to add here an interactive choiche
 cat(paste("Number of points available:",n_points_available))
-point=1 # <-- Select the point here ( value = 1, ... , n_point_available) 
+point = 1 # <-- Select the point here ( value = 1, ... , n_point_available) 
 out_new <- out[[point]] 
 
 # List the variable to plot
@@ -92,8 +77,8 @@ paste("Variables:",choices)
 #- Plot variables for single point -------------------------------------------------------------------------------------------------------------------------
 
 # --------------------------------------------------
-# Snow height plot
-input_variables=c("snow_depth.mm.",                                  # <-- Select variables here ( value = 1, ... , n_point_available)                       
+# (1) Snow height plot
+input_variables = c("snow_depth.mm.", # <-- Select variables here ( value = 1, ... , n_point_available)                       
                   "Psnow_over_canopy.mm.",
                   "Prain_over_canopy.mm.")
 mydata <- out_new[,input_variables] 
@@ -104,9 +89,16 @@ myfigure <- dygraph(mydata) %>%
 
 myfigure
 
+p <- ggplot(data = mydata,mapping = aes(x=as.POSIXct(index(mydata)), y=snow_depth.mm.))+
+  geom_line()+
+  geom_line(aes(y=Psnow_over_canopy.mm.), color="blue")+
+  geom_line(aes(y=Prain_over_canopy.mm.), color="red")
+p
+ggsave(plot = p, filename=file.path(wpath ,"out","1_snow_height.jpeg"), device = "jpeg") # the folder "out" MUST already be there!
+
 # --------------------------------------------------
-# Snow water equivalent plot
-input_variables=c("snow_water_equivalent.mm.",                                  # <-- Select variables here ( value = 1, ... , n_point_available)                       
+# (2) Snow water equivalent plot
+input_variables = c("snow_water_equivalent.mm.", # <-- Select variables here ( value = 1, ... , n_point_available)                       
                   "snow_melted.mm.",
                   "snow_subl.mm.")
 mydata <- out_new[,input_variables] 
@@ -117,61 +109,73 @@ myfigure <- dygraph(mydata) %>%
 
 myfigure
 
-# --------------------------------------------------
-# Snow density plot
-input_variables=c("snow_density.kg.m3.")
-mydata <- out_new[,input_variables] 
-
-myfigure <- dygraph(mydata) %>%
-  dyRangeSelector() %>%
-  dyRoller()
-
-myfigure
-
-# --------------------------------------------------
-# Snow temperature plot
-input_variables=c( "snow_temperature.C.")
-mydata <- out_new[,input_variables] 
-
-myfigure <- dygraph(mydata) %>%
-  
-  
-  dyRangeSelector() %>%
-  dyRoller()
-
-myfigure
-
-
-# --------------------------------------------------
-# Plots and saves one snow plot as  file
-
-
-point=4 
-out_new <- out[[point]] 
-input_variables=c("snow_depth.mm.",                                  # <-- Select variables here ( value = 1, ... , n_point_available)                       
-                  "Psnow_over_canopy.mm.",
-                  "Prain_over_canopy.mm.")
-mydata <- out_new[,input_variables] 
-
-
-p<-ggplot(data = mydata,mapping = aes(x =index(mydata),y=snow_depth.mm.))+
+p<-ggplot(data = mydata,mapping = aes(x=as.POSIXct(index(mydata)), y=snow_water_equivalent.mm.))+
   geom_line()+
-  geom_line(aes(y=Psnow_over_canopy.mm.),color="blue")+
-geom_line(aes(y=Prain_over_canopy.mm.),color="red")
+  geom_line(aes(y=snow_melted.mm.), color="blue")+
+  geom_line(aes(y=snow_subl.mm.), color="red")
 p
-ggsave(plot = p,filename = file.path(wpath ,"out","name.jpeg"),device = "jpeg")
+ggsave(plot = p, filename=file.path(wpath ,"out","2_snow_water_equivalent.jpeg"), device = "jpeg") 
 
 # --------------------------------------------------
-# Plots and saves many snow plot as  file
+# (3) Snow density plot
+input_variables = c("snow_density.kg.m3.")
+mydata <- out_new[,input_variables] 
 
-input_variables=c("snow_depth.mm.",                                  # <-- Select variables here ( value = 1, ... , n_point_available)                       
+myfigure <- dygraph(mydata) %>%
+  dyRangeSelector() %>%
+  dyRoller()
+
+myfigure
+
+# p<-ggplot(data = mydata,mapping = aes(x=as.POSIXct(index(mydata)), y=snow_density.kg.m3.))+
+#   geom_line()
+# p # ===> Error in FUN(X[[i]], ...) : oggetto "snow_density.kg.m3." non trovato
+# ggsave(plot = p, filename=file.path(wpath ,"out","3_snow_density.jpeg"), device = "jpeg") 
+
+# --------------------------------------------------
+# (4) Snow temperature plot
+input_variables = c( "snow_temperature.C.")
+mydata <- out_new[,input_variables] 
+
+myfigure <- dygraph(mydata) %>%
+  
+  
+  dyRangeSelector() %>%
+  dyRoller()
+
+myfigure
+
+# p<-ggplot(data = mydata,mapping = aes(x=as.POSIXct(index(mydata)), y=snow_temperature.C.))+
+#   geom_line()
+# p ===> Error in FUN(X[[i]], ...) : oggetto "snow_temperature.C." non trovato
+# ggsave(plot = p, filename=file.path(wpath ,"out","4_snow_temperature.jpeg"), device = "jpeg") 
+# --------------------------------------------------
+# (5) Plots and saves one snow plot as file
+point = 4 
+out_new <- out[[point]] 
+input_variables = c("snow_depth.mm.", # <-- Select variables here ( value = 1, ... , n_point_available)                       
                   "Psnow_over_canopy.mm.",
                   "Prain_over_canopy.mm.")
-lapply(seq(from=1,to=n_points_available,by=1),GEOtop_Plot_loop,out=out,input_variables=input_variables,"snow")
+mydata <- out_new[,input_variables] 
+
+
+p<-ggplot(data = mydata,mapping = aes(x=as.POSIXct(index(mydata)), y=snow_depth.mm.))+
+  geom_line()+
+  geom_line(aes(y=Psnow_over_canopy.mm.), color="blue")+
+geom_line(aes(y=Prain_over_canopy.mm.), color="red")
+p
+ggsave(plot = p, filename=file.path(wpath ,"out","5_snow_depth.jpeg"), device = "jpeg")
 
 # --------------------------------------------------
-# Et partitioning plot
-input_variables=c("Evap_surface.mm.",                                  # <-- Select variables here ( value = 1, ... , n_point_available)                       
+# (6) Plots and saves many snow plot as file
+input_variables = c("snow_depth.mm.", # <-- Select variables here ( value = 1, ... , n_point_available)                       
+                  "Psnow_over_canopy.mm.",
+                  "Prain_over_canopy.mm.")
+lapply(seq(from=1,to=n_points_available,by=1), GEOtop_Plot_loop, out=out, input_variables=input_variables,"6_snow_depth")
+# This should produce 9 pics but only one empty figure "6_snow1_depth.jpeg" is generated!
+# --------------------------------------------------
+# (7) Et partitioning plot
+input_variables = c("Evap_surface.mm.", # <-- Select variables here ( value = 1, ... , n_point_available)                       
                   "Trasp_canopy.mm.",
                   "Evapotranspiration.mm.")
 mydata <- out_new[,input_variables] 
@@ -180,8 +184,12 @@ dygraph(mydata) %>%
   dyRangeSelector() %>%
   dyRoller()
 
-
-
+p<-ggplot(data = mydata,mapping = aes(x=as.POSIXct(index(mydata)), y=Evap_surface.mm.))+
+  geom_line()+
+  geom_line(aes(y=Trasp_canopy.mm.), color="blue")+
+  geom_line(aes(y=Evapotranspiration.mm.), color="red")
+p
+ggsave(plot = p, filename=file.path(wpath ,"out","7_Evap_surface.jpeg"), device = "jpeg")
 
 
 
